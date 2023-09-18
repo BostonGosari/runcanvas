f70c32ccd733628f44f6c62b0b7ed2528da58410
chamroro <chamsae@handong.ac.kr>
2023-09-18 16:42:06 +0900

    [feat] 사전예약 폼

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,21 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Modal from "./Modal";
 function Home() {
-  const Home = styled.div``;
+  const Home = styled.div`
+    display: flex;
+    justify-content: center;
+    a {
+      border-radius: 30px;
+      background: #000;
+      color: #fff;
+      font-family: Pretendard;
+      font-size: 36px;
+      font-style: normal;
+      font-weight: 700;
+      text-align: center;
+      padding: 27px 74px;
+    }
+  `;
 
   const [isShowing, setIsShowing] = useState(false);
   const openModal = () => {
@@ -12,7 +26,7 @@ function Home() {
   return (
     <Home>
       <a className="apply" onClick={() => openModal()}>
-        사전신청하기
+        사전예약하기
       </a>
       <div>{isShowing != "" ? <Modal onClose={setIsShowing} /> : null}</div>
     </Home>
